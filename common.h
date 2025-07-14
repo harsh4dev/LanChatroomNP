35d4078658ea57d39af32e8a26aebcc171f4418e
Harry <harsh.221715@ncit.edu.np>
2025-07-14 20:58:07 +0545

    Add initial client, server, and common header files
    Created empty client.cpp and server.cpp files, and added common.h with basic includes, constants, and configuration for a Windows socket-based application.

---
+++ common.h
@@ -0,0 +1,21 @@
+// common.h
+#ifndef COMMON_H
+#define COMMON_H
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <winsock2.h>
+#include <windows.h>
+#include <iostream>
+#include <thread>
+#include <map>
+#include <vector>
+#include <fstream>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#define PORT 54000
+#define BUFFER_SIZE 1024
+#define MAX_CLIENTS 10
+
+#endif
